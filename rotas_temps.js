b512fc6cd57c541351808d36af6c3e565630f6c1
CDBIOT <crisdb2008@gmail.com>
2022-06-22 18:42:00 -0300

    _id-

--- rotas_temps.js
+++ rotas_temps.js
@@ -75,8 +75,8 @@ routers.patch('/temps/:id',async (req, res) =>{
 
  //Delete
 routers.delete('/temps/:id', async (req, res) => {
-    const _id= req.params.id
-    const temps = await Temps.findByIdAndDelete({id: _id})
+    const id= req.params.id
+    const temps = await Temps.findByIdAndDelete({id: id})
     if(!temps){
     res.status(422).json({message:  'Temperatura não encontrada'});
     return
