38d704b400c95662a4ade1df792aede9b18ce772
CDBIOT <cristiab05@yahoo.com.br>
2026-09-14 20:29:16 -0300

    8084

--- mqttio.js
+++ mqttio.js
@@ -17,7 +17,7 @@ const options = {
   password: 'test',
  reconnectPeriod: 1000,
 }
-const client  = mqtt.connect('wss://broker.mqtt-dashboard.com:8884/mqtt', options)
+const client  = mqtt.connect('wss://broker.mqtt-dashboard.com:8084/mqtt', options)
 client.on('connect', function () {
   console.log('Connected on mqtt broker')
 
